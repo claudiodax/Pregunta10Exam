1b93a527cbf0e040c28f4e5702952765f3a44b1d
claudiodax <claudio.zavala@tecsup.edu.pe>
2021-09-07 10:39:51 -0500

    Pregunta 10

--- Project02/App.js
+++ Project02/App.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
 import {StyleSheet,TouchableOpacity,Text,View,Image,TextInput, FlatList} from 'react-native';
-import OurFlatList from './App/components/ourFlatList/OurFlatList';
+import ConexionFetch from './App/components/conexionFetch/ConexionFetch';
 
 
 export default class App extends Component{
@@ -10,54 +10,11 @@ export default class App extends Component{
       textValue: '',
       textResult: '',
       count: 0,
-      Carros:[
-        {
-          id:1,
-          name: 'Carro rojo clasio',
-          img: 'https://www.elcarrocolombiano.com/wp-content/uploads/2021/01/20210124-LOS-10-CARROS-MAS-VENDIDOS-DEL-MUNDO-EN-2020-01-750x460.jpg',
-        },
-        {
-          id:2,
-          name: 'Carro rojo deportivo',
-          img: 'https://st1.uvnimg.com/d4/4a/006304a74db4902c0b4d8d8026c8/chevrolet-corvette-c8-stingray-2020-1280-08.jpg',
-        },
-        {
-          id:3,
-          name: 'Carro rojo familiar',
-          img: 'https://www.elcarrocolombiano.com/wp-content/uploads/2021/02/20210208-TOP-75-CARROS-MAS-VENDIDOS-DE-COLOMBIA-EN-ENERO-2021-01.jpg',
-        },
-        {
-          id:4,
-          name: 'Carro mistico',
-          img: 'https://img.autocosmos.com/noticias/fotosprinc/NAZ_b65480612b9249c0885a3ec88c5641e1.jpg',
-        },
-      ],
     }
   }
 
-changeTextInput = text => {
-  console.log(text);
-  this.setState({textValue: text});
-};
-onPress = () =>{
-  this.setState({
-    count: this.state.count + 1,
-  })
-};
-renderItem = ({item}) =>(
-  <TouchableOpacity>
-    <View style={styles.contendorLista}>
-      <Image style={styles.imagen} source={{uri:item.img}}/>
-      <Text style={styles.itemLista}>{item.name}</Text>
-    </View>
-  </TouchableOpacity>
-)
   render(){
-    return(
-      <View style={styles.container}>
-        <OurFlatList/>
-      </View>
-    );
+    return <ConexionFetch/> ;
   }
 }
 
